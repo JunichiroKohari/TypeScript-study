3ea888ef8d00fea6c7220b00cd4e9cb4f68a6360
小針隼一郎 <>
2020-03-24 20:58:59 +0900

    getter, setter

--- class/class.ts
+++ class/class.ts
@@ -15,7 +15,19 @@ class Person {
 }
 
 class Teacher extends Person {
-    constructor(name: string, age: number, public subject: string) {
+    get subject(): string {
+        if (!this._subject) {
+            throw new Error('There is no subject.');
+        }
+        return this._subject;
+    }
+    set subject(value) {
+        if (!value) {
+            throw new Error('There is no subject.');
+        }
+        this._subject = value;
+    }
+    constructor(name: string, age: number, private _subject: string) {
         super(name, age);
     }
 
@@ -25,4 +37,6 @@ class Teacher extends Person {
 }
 
 const teacher = new Teacher('Jun', 23, "Math");
+console.log(teacher.subject);
+teacher.subject = 'Music'
 teacher.greeting();
