1a7911fd2360ad0736ec30b7c46c3e3b283f6b10
小針隼一郎 <>
2020-03-26 21:53:02 +0900

    interfaceで関数の型を表現する方法

--- interface/interface.ts
+++ interface/interface.ts
@@ -1,3 +1,11 @@
+interface addFunc {
+    (num1: number, num2: number): number;
+}
+let addFunc: addFunc;
+addFunc = (n1: number, n2: number) => {
+    return n1 + n2;
+}
+
 interface Nameable {
     name: string;
 }
